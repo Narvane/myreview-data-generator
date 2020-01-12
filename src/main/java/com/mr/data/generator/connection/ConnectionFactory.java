7ac68c3b3257c3de5df2cf63fe1002f3c17f802b
Gustavo Alves de Andrade <gustavoalvesandrade18@gmail.com>
2020-01-12 16:42:30 -0300

    update Connection

--- src/main/java/com/mr/data/generator/connection/ConnectionFactory.java
+++ src/main/java/com/mr/data/generator/connection/ConnectionFactory.java
@@ -2,7 +2,7 @@ package com.mr.data.generator.connection;
 
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Map;
+import java.util.HashMap;
 
 public class ConnectionFactory {
     public static String server;
@@ -10,10 +10,11 @@ public class ConnectionFactory {
     public static String port;
     public static String username;
     public static String password;
-	public static java.sql.Connection doConnection(Map<String, String> databaseSelector) throws SQLException {
+    public static HashMap<String, String> databaseSelector = new HashMap<String, String>();
+	public static java.sql.Connection doConnection() throws SQLException {
 		try {
 			Class.forName(databaseSelector.get("driver"));
-			return DriverManager.getConnection(databaseSelector.get("db")+"ec2-54-221-212-126.compute-1.amazonaws.com:5432/dpgfi0t4oqaoj", "ofaqxvkwycbfjq", "2459cad22b0bf93bf313055cc36aa0c1985d94d9ffac85eec14874bb053a8559");
+			return DriverManager.getConnection(databaseSelector.get("db")+server+":"+port+"/"+database","+ username+","+ password);
 		} catch (ClassNotFoundException e) {
 			throw new SQLException(e.getException());
 		}
