3bfd28695849219609aa1a7a1b15c50dfc233a86
Gustavo Alves de Andrade <gustavoalvesandrade18@gmail.com>
2020-01-12 13:07:30 -0200

    Controller finished (No refactored)

--- src/main/java/com/mr/data/generator/controller/EstablishmentController.java
+++ src/main/java/com/mr/data/generator/controller/EstablishmentController.java
@@ -7,14 +7,20 @@ package com.mr.data.generator.controller;
 
 
 import com.mr.data.generator.connection.ConnectionFactory;
+import com.mr.data.generator.dao.EstablishmentDAO;
 import com.mr.data.generator.mode.Establishment;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 /**
  *
  * @author Guga
  */
 public class EstablishmentController {
+    
+    EstablishmentDAO establishmentDAO = new EstablishmentDAO();
+    
     private String db;
     private String server;
     private String database;
@@ -54,22 +60,51 @@ public class EstablishmentController {
     }
     
     public String randomName(){
-        return "";
+	Random r = new Random();
+	String prename = namesList.get(r.nextInt(namesList.size()));
+	String afterName = afternameList.get(r.nextInt(afternameList.size()));
+		
+	while (prename != afterName) {
+            String name = prename + " " + afterName;
+            return name;
+	}
+	return randomName();
     }
     public String randomCnpj(){
-        return "";
+        Random r = new Random();
+	Random r2 = new Random();
+		
+	int a = r.nextInt(8000000)+1000000;
+	int b = r2.nextInt(8000000)+1000000;
+	String total = a +""+ b;
+	return total;
     }
     public Integer randomDistrict(){
-        return 0;
+        if(allDistricts){
+            Random r = new Random();
+            return establishmentDAO.getAllDistrictsIds().get(r.nextInt(establishmentDAO.getAllDistrictsIds().size()));
+        } 
+        Random r = new Random();
+        return establishmentDAO.getDistrictByNames(districtNames).get(r.nextInt(districtNames.size()));
     }
     public Integer randomNumber(){
-        return 0;
+        Random r = new Random();
+	return r.nextInt(9999);
     }
     public String randomDescription(){
-        return "";
+        Random r = new Random();
+        ArrayList<String> descriptions = new ArrayList<>();
+        descriptions.add("Nice Place to Eat");
+        descriptions.add("Italian Restaurant");
+        descriptions.add("Mexican Restaurant");
+        descriptions.add("Fast Food");
+        descriptions.add("Gourmet");
+        return descriptions.get(r.nextInt());
     }
     public Double randomStars(){
-        return 0.0;
+            Random r = new Random();
+            String join = r.nextInt(5) + "." + r.nextInt(5);
+	return Double.valueOf(join);
     }
     public void sqlSubscribe(Establishment establishment){
         if(db=="Mysql"){
