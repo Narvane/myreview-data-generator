0acc85ba2fc2b5a5e45538d1b9c401e9a43abb2c
Gustavo Alves de Andrade <gustavoalvesandrade18@gmail.com>
2020-01-12 16:04:35 -0300

    add getAllDistrictsAdd

--- src/main/java/com/mr/data/generator/dao/EstablishmentDAO.java
+++ src/main/java/com/mr/data/generator/dao/EstablishmentDAO.java
@@ -17,6 +17,23 @@ public class EstablishmentDAO {
     
     public ArrayList<Integer> getAllDistrictsIds(){
         ArrayList<Integer> idList = new ArrayList<>();
+        
+        String sql = "SELECT * FROM dst_districts";
+		
+		try {
+			connection = ConnectionFactory.doConnection();
+			PreparedStatement stmt = connection.prepareStatement(sql);
+
+			ResultSet rs = stmt.executeQuery();
+			while (rs.next()) {
+				idList.add(rs.getInt("dst_id"));
+			}
+			
+			connection.close();
+			stmt.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
         return idList;
     }
     public ArrayList<Integer> getDistrictByNames(ArrayList<String> districtNames){
@@ -27,4 +44,25 @@ public class EstablishmentDAO {
     public void getAllCnpjs(){
         
     }
+    
+    public boolean districtExists(String districtName) {
+		String sql = "SELECT dst_name FROM dst_districts WHERE dst_name = ?";
+		
+		try {
+			connection = ConnectionFactory.doConnection();
+			PreparedStatement stmt = connection.prepareStatement(sql);
+			stmt.setString(1, districtName);
+
+			ResultSet rs = stmt.executeQuery();
+			if (rs.next()) {
+				return true;
+			}
+			
+			connection.close();
+			stmt.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
 }
